ebf9857556953c22fb16993c679466d37f169055
OonoYouji <k023g0007@g.neec.ac.jp>
2023-11-01 12:15:49 +0900

    コメントを消去

--- PM1_gitTest/main.cpp
+++ PM1_gitTest/main.cpp
@@ -71,8 +71,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Novice::ScreenPrintf(100, 150, "プルリクエストの作成");
 
 
-		Novice::ScreenPrintf(0, 0, "コンフリクトを防止");
-
 		///
 		/// ↑描画処理ここまで
 		///
